2bfe3124327777e8ab53401db8e12a31c0cfca3e
LENOVO <sasikrishna97@gmail.com>
2024-02-01 06:28:25 +0530

    updated code

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features = "src/test/java/features",
         glue="step_definitions"   ,
         plugin = {
-                "json:target/jsonReports/cucumber.json"
+                "json:src/test/resources/jsonReports/cucumber.json"
         },
         tags = {"@Regression"}
 )
