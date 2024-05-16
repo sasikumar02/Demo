7e06b8a408ae3c0ff8c008cb00714133933ab5a5
LENOVO <sasikrishna97@gmail.com>
2024-05-16 11:26:08 +0530

    updated pom.xml file and added Browser stack video link to the hooks

--- src/main/java/pages/DriverFactory.java
+++ src/main/java/pages/DriverFactory.java
@@ -1,6 +1,9 @@
 package pages;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -10,6 +13,7 @@ import java.net.URL;
 
 public class DriverFactory {
    public static  WebDriver driver;
+    public static String publicURL;
     public static final String USERNAME = "kannanre_3IAdJY";
     public static final String AUTOMATE_KEY = "Nx8ZDFbUtWU49hoMQtsS";
     public static final String url = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
@@ -20,10 +24,15 @@ public class DriverFactory {
         caps.setCapability("os_version", "10");
         caps.setCapability("browser", "Chrome");
         caps.setCapability("browser_version", "80");
-
         caps.setCapability("name", "sasikumar's First Test");
+        driver = new RemoteWebDriver(new URL(url), caps);
+        JavascriptExecutor jse = (JavascriptExecutor)driver;
+        Object response = jse.executeScript("browserstack_executor: {\"action\": \"getSessionDetails\"}");
+        JSONObject json = (JSONObject) new JSONParser().parse((String) response);
+        publicURL = (String) json.get("public_url");
+        System.out.println("session id -- before " + publicURL);
 
-         driver = new RemoteWebDriver(new URL(url), caps);
+
 // Navigating through the URL
 //        driver.get("http://www.google.com");
 ////Locating the search box of google
--- src/test/java/step_definitions/ServiceClass.java
+++ src/test/java/step_definitions/ServiceClass.java
@@ -19,5 +19,64 @@ public class ServiceClass extends BaseClass {
     @io.cucumber.java.After
     public void after() throws Exception {
         closeBrowser(scenario);
+        System.out.println("after step--"+publicURL);
+        scenario.attach(publicURL,"text/plain","Browser Stack video Link");
     }
+
+//    @io.cucumber.java.After
+//    public void sendEmail(){
+//        Properties properties = new Properties();
+////        properties.put("mail.smtp.host", "smtp.gmail.com"); // Change to your SMTP server
+////        properties.put("mail.smtp.socketFactory.port", "465");
+////        properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+////        properties.put("mail.smtp.auth", "true");
+////        properties.put("mail.smtp.port", "465"); // Change to your SMTP port
+//
+//        properties.put("mail.smtp.auth", "true");
+//        properties.put("mail.smtp.starttls.enable", "true");
+//        properties.put("mail.smtp.host", "smtp.gmail.com");
+//        properties.put("mail.smtp.port", "587");
+//
+//        // Step 3: Authenticate with the SMTP server
+//        Authenticator authenticator = new Authenticator() {
+//            protected PasswordAuthentication getPasswordAuthentication() {
+//                return new PasswordAuthentication("kannansasi1997@gmail.com", "Raeskasa1997@"); // Change to your email credentials
+//            }
+//        };
+//
+//        // Step 4: Create a session
+//        Session session = Session.getInstance(properties, authenticator);
+//
+//        try {
+//            // Step 5: Create a MimeMessage
+//            Message message = new MimeMessage(session);
+//            message.setFrom(new InternetAddress("kannansasi1997@gmail.com")); // Change to your email address
+//            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("sasikumar.re97@gmail.com")); // Change to recipient's email address
+//            message.setSubject("Cucumber Report");
+//
+//            // Step 6: Create the message body
+//            BodyPart messageBodyPart = new MimeBodyPart();
+//            messageBodyPart.setText("Please find attached cucumber report.");
+//
+//            // Step 7: Attach the HTML report
+//            MimeBodyPart attachmentBodyPart = new MimeBodyPart();
+//            attachmentBodyPart.attachFile("target/cucumber-reports/cucumber-html-reports/overview-features.html");
+//
+//            // Step 8: Create multipart
+//            Multipart multipart = new MimeMultipart();
+//            multipart.addBodyPart(messageBodyPart);
+//            multipart.addBodyPart(attachmentBodyPart);
+//
+//            // Step 9: Set multipart as content of message
+//            message.setContent(multipart);
+//
+//            // Step 10: Send the message
+//            Transport.send(message);
+//
+//            System.out.println("Email sent successfully.");
+//
+//        } catch (MessagingException | IOException e) {
+//            e.printStackTrace();
+//        }
+
 }
